a04ae6a4273aadd1196b48a5b32984c311ae12db
Rebecca Rossman <rrossman25@gmail.com>
2019-06-20 12:26:41 -0400

    Sequelize Essentials: check

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const html = require('html-template-tag');
+const { addPage, editPage, main, userList, userPages, wikiPage, layout } = require('./views/index')
+const { db, Page, User } = require('./models/index');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-   res.send('hello world');
+   res.send(layout('hello world'));
 })
+
 const PORT = 1337;
-app.listen(PORT, () => {
- console.log(`App listening in port ${PORT}`);
-});
+
+const init = async () => {
+   await db.sync({force: true});
+
+   app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+   });
+}
+
+init();
